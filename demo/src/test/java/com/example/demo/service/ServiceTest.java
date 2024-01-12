3a248b531b64d7a3d41388d0cb3d1d598ba9bbee
YeremiPolanco <yeremi.polanco.julca@gmail.com>
2024-01-12 12:49:22 -0500

    cambio leve prueba

---
+++ demo/src/test/java/com/example/demo/service/ServiceTest.java
@@ -0,0 +1,13 @@
+package com.example.demo.service;
+
+import org.junit.jupiter.api.Test;
+
+public class ServiceTest {
+	private final PatientDao patientDao;
+	
+	@Test
+	public void nombreTest() {
+		
+	}
+
+}
