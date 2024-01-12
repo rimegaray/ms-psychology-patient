0ba14e252264c750469cdd4d240990563ff42219
YeremiPolanco <yeremi.polanco.julca@gmail.com>
2024-01-12 18:51:51 -0500

    prueba de unit test

---
+++ demo/src/test/java/com/example/demo/service/PatientServiceTest.java
@@ -0,0 +1,97 @@
+package com.example.demo.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.Date;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.example.demo.repository.dao.PatientDao;
+import com.example.demo.repository.dto.PatientDto;
+import com.example.demo.repository.entity.PatientEntity;
+
+@ExtendWith(MockitoExtension.class)
+public class PatientServiceTest {
+	
+	
+	private static PatientService patientService;
+	
+	@Mock
+	PatientDao patientDaoMock;
+	
+	@BeforeEach
+	public void beforeAll() {
+		 patientService = new PatientService(patientDaoMock);
+	}
+	
+	@Test
+	public void addProductShouldReturnSuccessfulWhenDaoIsOk() {
+		//Precondiciones
+		byte[] myFileTest = new byte[10];
+		
+		PatientDto patientDtoExpected = new PatientDto();
+		patientDtoExpected.setFullName("Yeremi");
+		patientDtoExpected.setDni(76351126);
+		patientDtoExpected.setAge(17);
+		patientDtoExpected.setContactNumber(912923412);
+		patientDtoExpected.setAddress("la casa del raton");
+		patientDtoExpected.setEmail("yeremi.elraton@gmail.com");
+		patientDtoExpected.setOccupation("Estudiante");
+		patientDtoExpected.setDateOfAdmission(new Date("12/01/24"));
+		patientDtoExpected.setLifeStory(myFileTest);
+		patientDtoExpected.setObservations("Esta locuaz");
+		
+		PatientEntity patientEntity = new PatientEntity();
+		patientEntity.setIdPatient(0);
+		patientEntity.setFullName("Yeremi");
+		patientEntity.setDni(76351126);
+		patientEntity.setAge(17);
+		patientEntity.setContactNumber(912923412);
+		patientEntity.setAddress("la casa del raton");
+		patientEntity.setEmail("yeremi.elraton@gmail.com");
+		patientEntity.setOccupation("Estudiante");
+		patientEntity.setDateOfAdmission(new Date("12/01/24"));
+		patientEntity.setLifeStory(myFileTest);
+		patientEntity.setObservations("Esta locuaz");
+		
+		
+		PatientEntity patientEntityResult = new PatientEntity();
+		patientEntityResult.setIdPatient(0);
+		patientEntityResult.setFullName("Yeremi");
+		patientEntityResult.setDni(76351126);
+		patientEntityResult.setAge(17);
+		patientEntityResult.setContactNumber(912923412);
+		patientEntityResult.setAddress("la casa del raton");
+		patientEntityResult.setEmail("yeremi.elraton@gmail.com");
+		patientEntityResult.setOccupation("Estudiante");
+		patientEntityResult.setDateOfAdmission(new Date("12/01/24"));
+		patientEntityResult.setLifeStory(myFileTest);
+		patientEntityResult.setObservations("Esta locuaz");
+		Mockito.when(patientDaoMock.save(Mockito.any()))
+		  .thenReturn(patientEntityResult);
+		
+		//Ejecucion
+		PatientDto patientDto = patientService.addProduct(patientEntity);
+		
+		//Asserts
+		assertNotNull(patientDto);
+		assertEquals(patientDto.getFullName(), patientDtoExpected.getFullName());
+		assertEquals(patientDto.getFullName(), patientDtoExpected.getFullName());
+		assertEquals(patientDto.getDni(), patientDtoExpected.getDni());
+		assertEquals(patientDto.getAge(), patientDtoExpected.getAge());
+		assertEquals(patientDto.getContactNumber(), patientDtoExpected.getContactNumber());
+		assertEquals(patientDto.getAddress(), patientDtoExpected.getAddress());
+		assertEquals(patientDto.getEmail(), patientDtoExpected.getEmail());
+		assertEquals(patientDto.getOccupation(), patientDtoExpected.getOccupation());
+		assertEquals(patientDto.getDateOfAdmission(), patientDtoExpected.getDateOfAdmission());
+		assertEquals(patientDto.getLifeStory(), patientDtoExpected.getLifeStory());
+		assertEquals(patientDto.getObservations(), patientDtoExpected.getObservations());
+	}
+
+}
